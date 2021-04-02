e0b48eebcc8806d90c2662f0aa6f9a7e9830e0a5
Giovanna Souza <81442577+giosouzaa@users.noreply.github.com>
2021-04-02 12:00:05 -0300

    Create numero_primo.cpp

---
+++ exercicios/comando_repeticao/numero_primo.cpp
@@ -0,0 +1,30 @@
+/*
+Peça ao usuário que insira um número inteiro e diga a ele se o número escolhido é primo ou não é.
+
+Obs: Número primo é aquele que só é divisível por 1 e por ele mesmo.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+  
+	int n,i,divisores;
+  
+	printf("\nInsira um numero:\n");
+	scanf("%d",&n);
+	divisores=0;
+  
+	for(i=1;i<=n;i++){
+		if(n%i==0)
+		divisores=divisores+1;
+	}
+  
+	if(divisores>2)
+	printf("\nO numero %d nao eh primo\n\n",n);
+	else
+	printf("\nO numero %d eh primo\n\n",n);
+  
+	system("pause");
+	return 0;
+}
