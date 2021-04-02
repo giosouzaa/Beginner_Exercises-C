1e0e652f76d06888a62f5304d88768a9faac758c
Giovanna Souza <81442577+giosouzaa@users.noreply.github.com>
2021-04-02 14:59:28 -0300

    Update and rename criando_uma_matriz.cpp to 006.cpp

--- exercicios/vetores_strings_matrizes/006.cpp
+++ exercicios/vetores_strings_matrizes/006.cpp
@@ -1,7 +1,11 @@
 /*
-Peça ao usuário que insira os elementos da matriz e, em seguida, imprima na tela os elementos da diagonal da matriz.
 
-Obs: Correspondem à diagonal da matriz os elementos cuja posição na vertical (linha) é igual à da horizontal (coluna) (ex: [0][0]).
+006 - Criando uma matriz
+	Peça ao usuário que insira os elementos da matriz e, em seguida, imprima na tela os elementos da diagonal da matriz.
+
+	Obs: Correspondem à diagonal da matriz os elementos cuja posição na vertical (linha) é igual à da horizontal (coluna).
+	     ex.: [0][0].
+
 */
 
 #include<stdio.h>
@@ -18,7 +22,7 @@ int main (){
 		}
 	}
 	
-  for(i=0;i<4;i++){
+        for(i=0;i<4;i++){
 		for(j=0;j<4;j++){
 			if(i==j){
 				printf("\nm[%d,%d]=%d",i,j,m[i][j]);
