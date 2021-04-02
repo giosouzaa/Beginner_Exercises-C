a33a96ab9318cf9db6691e14e359e9c32fae8643
Giovanna Souza <81442577+giosouzaa@users.noreply.github.com>
2021-04-02 11:53:45 -0300

    Create tabuada(for).cpp

---
+++ exercicios/comando_repeticao/tabuada(for).cpp
@@ -0,0 +1,24 @@
+/*
+Urilizando a função for, peça ao usuário um número inteiro e imprima na tela sua tabuada de 0 a 10.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+  
+	int n,i,p;
+  
+	printf("\nInsira um numero inteiro:\n");
+	scanf("%d",&n);
+	printf("\nTabuada do numero %d:\n\n",n);
+
+  for(i=0;i<=10;i++){
+		p=n*i;
+		printf("%dx%d=%d\n",n,i,p);
+	}
+  
+	printf("\n");
+	system("pause");
+	return 0;
+}
