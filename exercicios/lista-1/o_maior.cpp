45a19eea4a1a18c67d1ad4dd6e7a5deaabb10d08
Giovanna Souza <81442577+giosouzaa@users.noreply.github.com>
2021-04-02 11:28:51 -0300

    Update o_maior.cpp

--- exercicios/lista-1/o_maior.cpp
+++ exercicios/lista-1/o_maior.cpp
@@ -1,27 +1,26 @@
 /*
-Peça ao usúario 10 números inteiros e retorne o maior entre eles.
+Utilizando a função while, peça ao usúario 10 números inteiros e retorne o maior entre eles.
 */
 
 #include<stdio.h>
 #include<stdlib.h>
 
 
-main ()
-{
+main (){
 	int n,maior,i;
 	printf("\nInsira 10 numeros inteiros:\n");
 	
 	i=1;
 	maior=0;
 	
-	while(i<=10)
-	{
+	while(i<=10){
 		scanf("%d",&n);
 		if (n>maior)
 		maior=n;
 		
 		i++;
 	}
+	
 	printf("\n\nO maior numero dentre os 10 inseridos eh %d\n\n",maior);
 	system ("pause");
 	return 0;
